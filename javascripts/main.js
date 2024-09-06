44e8d6a2b2321caab9f29546f996758242ab5f50
Dylanrusere <dylaneight01@gmail.com>
2024-09-06 16:39:47 +0200

    done

--- javascripts/main.js
+++ javascripts/main.js
@@ -41,3 +41,33 @@ window.onclick = function(event) {
         });
     }
 }
+
+// Get Quote Form
+// Get the modal
+const quoteForm = document.getElementById('quoteForm');
+
+// Get the button that opens the modal
+const quoteBtn = document.querySelectorAll('.quote_btn, .quote_btn_ham');
+
+// Get the <span> element that closes the modal
+const closeBtn = document.getElementsByClassName('close_btn')[0];
+
+// When the user clicks the button, open the modal 
+quoteBtn.forEach(btn => {
+    btn.onclick = function(event) {
+        event.preventDefault();
+        quoteForm.style.display = 'block';
+    };
+});
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+    quoteForm.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == quoteForm) {
+        quoteForm.style.display = 'none';
+    }
+};
